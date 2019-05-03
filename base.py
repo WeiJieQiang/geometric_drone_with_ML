7f7968fb5c9c8e64b90ddc5edf01ac33e4a49cdd
JieQiang (Jay) Wei <38379048+WeiJieQiang@users.noreply.github.com>
2019-05-03 11:38:48 +0200

    Speed up version 2, with analytic Omega_ddot.

--- base.py
+++ base.py
@@ -4,24 +4,27 @@ Created on Wed Nov 14 15:31:14 2018
 
 @author: jieqiang (jieqiang.wei@gmail.com)
 
-This is the code based on paper "Geometric tracking control of a quadrotor UAV on SE(3)". with some machine learning extension.
+The tracking part of the code is based on "Geometric tracking control of a quadrotor UAV on SE(3)". As a source of
+uncertainty, we assume that the mass used in the controller is different from the actual mass. This could be due to
+measurement noise, or even mass drop during flight. We identify the model uncertainty using Gaussian process and Neural
+network.
 """
-"""
+"""This code has no numerical approximations, but everything is calculated analytically."""
 
-"""
 
 import numpy as np
 #import quaternion
 from scipy.integrate import solve_ivp
-from numpy import linalg as LA
+from numpy.linalg import norm as no
 from functools import reduce
 import mat_fun as mf
 import time
 
 
 class Drone(object):
-    def __init__(self, m):
-        self.mass = m
+    def __init__(self, m_true, m_controller):
+        self.mass_sys = m_true
+        self.mass_ctrl = m_controller
         self.gravity = 9.81
         self.inertia = np.diag([0.0820,0.0845,0.1377])
 
@@ -29,7 +32,7 @@ class Drone(object):
         #self.state = np.array[self.p, self.v.tolist(), self.r.tolist(), self.w.tolist()]
         self.state = np.array([0., 0., 0.,0., 0., 0.,1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0.])
 
-        self.t_prev = 0.1
+        self.t_prev = 0
 
         self.Omega_d_prev = np.array([0.,0.,0.])
 
@@ -61,89 +64,107 @@ class Drone(object):
         v_d = np.array([0.4, 0.4 * np.pi * np.cos(np.pi * t), -0.6 * np.pi * np.sin(np.pi * t)])
         x_ddotdot = np.array([0, -0.4 * np.pi ** 2 * np.sin(np.pi * t), -0.6 * np.pi ** 2 * np.cos(np.pi * t)])
         x_ddotdotdot = np.array([0, -0.4 * np.pi ** 3 * np.cos(np.pi * t), 0.6 * np.pi ** 3 * np.sin(np.pi * t)])
-        return x_d,v_d,x_ddotdot,x_ddotdotdot
+        x_ddotdotdotdot = np.array([0, 0.4 * np.pi ** 4 * np.sin(np.pi * t), 0.6 * np.pi ** 4 * np.cos(np.pi * t)])
+        return x_d,v_d,x_ddotdot,x_ddotdotdot,x_ddotdotdotdot
 
     def differential_b_1d(self,t):
         # the desired direction b_1d
         b_1d = np.array([np.cos(np.pi * t), np.sin(np.pi * t), 0])
         b_1ddot = np.array([-np.pi * np.sin(np.pi * t), np.pi * np.cos(np.pi * t), 0])
-        return b_1d,b_1ddot
+        b_1ddotdot = np.array([- (np.pi **2) * np.cos(np.pi * t), - (np.pi**2) * np.sin(np.pi * t), 0])
+        return b_1d,b_1ddot,b_1ddotdot
 
 
     def controller(self, t, y):
 
 
-        dt = max(t-self.t_prev,0.01)
+        dt = max(t-self.t_prev,0.01)#no if statement for code speed up.
+
+        # dt = t-self.t_prev
+        # if dt < 0.01:
+        #     dt = 0.01
+        #print "dt",dt
 
         self.t_prev = t
 
-        #p = y[0:3]
-        #v = y[3:6]
-        #R = y[6:15].reshape(3, 3, order='F')
-        #Omega = y[15:18]
-        #Omega_hat = mf.w_hat(y[15:18])
+        p = y[0:3]
+        v = y[3:6]
+        R = y[6:15].reshape(3, 3, order='F')
+        Omega = y[15:18]
+        Omega_hat = mf.w_hat(Omega)
 
 
         # calculation based on the desired direction b_1d
-        b_1d,b_1ddot = self.differential_b_1d(t)
+        b_1d,b_1ddot,b_1ddotdot = self.differential_b_1d(t)
 
         # calculation based on the desired trajectory x_d
-        x_d,v_d,x_ddotdot,x_ddotdotdot = self.differential_x_d(t)
+        x_d,v_d,x_ddotdot,x_ddotdotdot,x_ddotdotdotdot = self.differential_x_d(t)
 
         # the errors: e_x e_v
-        e_x = y[0:3] - x_d
-        e_v = y[3:6] - v_d
+        e_x = p - x_d
+        e_v = v - v_d
 
         # to calculate b_3d
         e_3 = np.array([0., 0., 1.])
-        b_3d_temp = -k_x * e_x - k_v * e_v - self.mass * self.gravity * e_3 + self.mass * x_ddotdot
-        b_3d = -b_3d_temp / LA.norm(b_3d_temp)
+        b_3d_temp = -k_x * e_x - k_v * e_v - self.mass_ctrl * self.gravity * e_3 + self.mass_ctrl * x_ddotdot
+        b_3d = -b_3d_temp / no(b_3d_temp)
 
         # control input f
-        f = - np.dot(b_3d_temp, np.dot(y[6:15].reshape(3, 3, order='F'), e_3))
+        f = - np.dot(b_3d_temp, np.dot(R, e_3))
 
         # construct R_d
         b_2d_temp = np.cross(b_3d_temp, b_1d)  # this is different from the paper
-        b_2d = b_2d_temp / LA.norm(b_2d_temp)
+        b_2d = b_2d_temp / no(b_2d_temp)
 
-        R_d = reduce(np.append, [np.cross(b_2d, b_3d), b_2d, b_3d]).reshape(3, 3,
-                                                                            order='F')
+        R_d = reduce(np.append, [np.cross(b_2d, b_3d), b_2d, b_3d]).reshape(3, 3, order='F')
+
         # calculate \dot{R}_d: R_ddot
         b_3d_temp_dot = -k_x * e_v - k_v * (
-                self.gravity * e_3 - f * np.dot(y[6:15].reshape(3, 3, order='F'), e_3) / self.mass - x_ddotdot) + self.mass * x_ddotdotdot
+                self.gravity * e_3 - f * np.dot(R, e_3) / self.mass_ctrl - x_ddotdot) + self.mass_ctrl * x_ddotdotdot
         b_2d_temp_dot = np.cross(b_3d_temp_dot, b_1d) + np.cross(b_3d_temp, b_1ddot)
-        b_3ddot = LA.norm(b_3d_temp) ** (-3) * np.dot(np.dot(b_3d_temp.reshape(3, 1), b_3d_temp.reshape(1, 3))
-                                                      - LA.norm(b_3d_temp) ** 2 * np.identity(3), b_3d_temp_dot)
-        b_2ddot = LA.norm(b_2d_temp) ** (-3) * np.dot(-np.dot(b_2d_temp.reshape(3, 1), b_2d_temp.reshape(1, 3))
-                                                      + LA.norm(b_2d_temp) ** 2 * np.identity(3), b_2d_temp_dot)
-        R_ddot = reduce(np.append, [np.cross(b_2ddot, b_3d) + np.cross(b_2d, b_3ddot), b_2ddot, b_3ddot]).reshape(3, 3,
-                                                                                                                  order='F')
+        b_3ddot = -mf._derive_normed_vector(b_3d_temp,b_3d_temp_dot)
+        b_2ddot = mf._derive_normed_vector(b_2d_temp,b_2d_temp_dot)
+        R_ddot = reduce(np.append, [np.cross(b_2ddot, b_3d) + np.cross(b_2d, b_3ddot), b_2ddot, b_3ddot]).reshape(3, 3, order='F')
 
         # calculate Omega_d
         Omega_d_hat = np.dot(R_d.transpose(), R_ddot)
         Omega_d = mf.matrix_hat_inv(Omega_d_hat)
 
+
+        # calculate e_R, e_Omega
+        e_R = 0.5 * mf.matrix_hat_inv( np.dot(R_d.transpose(), R) - np.dot(R.transpose(), R_d) )
+        e_Omega = Omega - reduce(np.dot, [R.transpose(), R_d, Omega_d])
+
+
+        # calculate Omega_ddot numerically
+        #Omega_ddot = (Omega_d-self.Omega_d_prev)/dt
+
+        #self.Omega_d_prev = Omega_d
+
         # calculate Omega_ddot analytically
-        #b_2ddotdot = LA.norm(b_2d_temp)**(-6) * ( LA.norm(b_2d_temp)**(3)*()  - 3*LA.norm(b_2d_temp) )
+        ddv = - (np.dot(b_3d_temp,reduce(np.dot,[R,Omega_hat, e_3])) * np.dot(R,e_3)
+                #reduce(np.dot,[R,Omega_hat, e_3]) * np.dot(b_3d_temp, np.dot(R,e_3))
+                 + np.dot(R,e_3) * np.dot(b_3d_temp_dot, np.dot(R,e_3))
+                 + np.dot(R,e_3) * np.dot(b_3d_temp, reduce(np.dot, [R,Omega_hat, e_3])))/self.mass_ctrl
+        b_3d_temp_dotdot = (- k_x * (self.gravity * e_3 - f * np.dot(R, e_3) / self.mass_ctrl - x_ddotdot)
+                            - k_v * (ddv - x_ddotdotdot)  + self.mass_ctrl * x_ddotdotdotdot)
 
+        b_3ddotdot = -mf._derive_derived_normed_vector(b_3d_temp,b_3d_temp_dot,b_3d_temp_dotdot)
 
-        # calculate e_R
-        e_R = 0.5 * mf.matrix_hat_inv( np.dot(R_d.transpose(), y[6:15].reshape(3, 3, order='F')) - np.dot(y[6:15].reshape(3, 3, order='F').transpose(), R_d) )
+        b_2d_temp_dotdot = np.cross(b_3d_temp_dotdot, b_1d) + np.cross(b_3d_temp_dot, b_1ddot) \
+                           + np.cross(b_3d_temp_dot, b_1ddot) + np.cross(b_3d_temp, b_1ddotdot)
+        b_2ddotdot = mf._derive_derived_normed_vector(b_2d_temp,b_2d_temp_dot,b_2d_temp_dotdot)
 
+        Rd_1ddotdot = np.cross(b_2ddotdot, b_3d) + np.cross(b_2ddot, b_3ddot) + np.cross(b_2ddot, b_3ddot) + np.cross(b_2d, b_3ddotdot)
 
-        e_Omega = y[15:18] - reduce(np.dot, [y[6:15].reshape(3, 3, order='F').transpose(), R_d, Omega_d])
+        R_ddotdot = reduce(np.append, [Rd_1ddotdot,b_2ddotdot,b_3ddotdot]).reshape(3, 3, order='F')
 
-
-        # calculate Omega_ddot, use numerical not analytic method
-        Omega_ddot = (Omega_d-self.Omega_d_prev)/dt
-
-        self.Omega_d_prev = Omega_d
-
+        Omega_ddot = mf.matrix_hat_inv(np.dot(R_d.transpose(),(R_ddotdot-np.dot(R_ddot,Omega_d_hat))))
 
 
         # control input
-        tau = -k_R*e_R - k_Omega * e_Omega + reduce(np.dot,[mf.w_hat(y[15:18]),self.inertia,y[15:18]])\
-              - np.dot(self.inertia,(reduce(np.dot,[mf.w_hat(y[15:18]),y[6:15].reshape(3, 3, order='F').transpose(),R_d,Omega_d])-reduce(np.dot,[y[6:15].reshape(3, 3, order='F').transpose(),R_d,Omega_ddot])))
+        tau = -k_R*e_R - k_Omega * e_Omega + reduce(np.dot,[Omega_hat,self.inertia,Omega])\
+              - np.dot(self.inertia,(reduce(np.dot,[Omega_hat,R.transpose(),R_d,Omega_d])-reduce(np.dot,[R.transpose(),R_d,Omega_ddot])))
 
 
         return f, tau, Omega_d
@@ -171,7 +192,7 @@ class Drone(object):
         # velocity
         state_dot[0:3] = y[3:6]
         # acceleration
-        state_dot[3:6] = np.array([0., 0., self.gravity] - f*np.dot(y[6:15].reshape(3, 3, order='F'), e_3)/self.mass)
+        state_dot[3:6] = np.array([0., 0., self.gravity] - f*np.dot(y[6:15].reshape(3, 3, order='F'), e_3)/self.mass_sys)
         state_dot[6:15] = np.dot(y[6:15].reshape(3, 3, order='F'), Omega_hat).reshape(9, order='F')
         state_dot[15:18] = np.dot(np.linalg.inv(self.inertia), np.dot(np.dot(-Omega_hat, self.inertia), y[15:18]) + tau)
 
@@ -184,13 +205,13 @@ import numpy as np
 
 
 t_start = 0
-t_stop = 10
+t_stop = 20
 t_step = 0.05
 el = int((t_stop-t_start)/t_step)
 t_list = np.linspace(t_start, t_stop, el)
 
 
-drone = Drone(m=4.34)
+drone = Drone(m_true=4.34,m_controller=4.34)
 
 # the control gains
 global k_x,k_v,k_R,k_Omega
@@ -240,7 +261,7 @@ for i in xrange(len(time_update)):
 
 
 
-# the plots zone
+""" the plots zone """
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -294,3 +315,7 @@ else:
     print "plot method has to be 3D or 2D!"
 
 plt.show()
+
+"""Data collection part"""
+
+"""ML part"""
